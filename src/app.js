e4bccc5481e19c85666621818669ed0eaa72725e
Meltje23 <meltje23@gmail.com>
2023-03-02 05:41:39 +0100

    Added days weather forecast in javascript

--- src/app.js
+++ src/app.js
@@ -21,6 +21,57 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+
+              <div class="col-2">
+                <div class="weather-forecast-date">Thu</div>
+
+                <img
+                  src="http://openweathermap.org/img/wn/09n@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18° </span>
+                  <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>
+           
+`;
+  });
+
+  forecastHTML =
+    forecastHTML +
+    `
+
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+
+                <img
+                  src="http://openweathermap.org/img/wn/09n@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18° </span>
+                  <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>
+           
+`;
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -90,3 +141,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
